0a11f7835c99f5699e11adb37920e875a2fa8981
KAIVALYACHAVAN <>
2022-02-04 04:48:44 +0000

    Explore working

--- Explore1.cpp
+++ Explore1.cpp
@@ -8,15 +8,20 @@ struct Point {
 int main() {
     // First instance of Point, named a
     Point a;
-    a.x = 3;
+    a.x = 2;
     a.y = 9;
     // Second instance of Point, named b
     Point b;
     b.x = 4.5;
     b.y = 7.8;
 
+    Point c;
+    c.x = 9.2;
+    c.y = 2.3;
+
     cout << "Point a: x = " << a.x << ", y = " << a.y << endl;
     cout << "Point b: x = " << b.x << ", y = " << b.y << endl;
+    cout << "Point c: x = " << c.x << ", y = " << c.y << endl;
 
     // Add your changes here.
 
